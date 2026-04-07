49776b56a6158d04d4dcb2bd8ab8593022ba51ca
Admin <admin@minisupermarket.com>
2026-04-07 16:57:41 +0700

    Sửa excel 	new file:   .vscode/settings.json 	modified:   pom.xml 	renamed:    src/main/java/entity/ToHopMon.java -> src/main/java/dal/entities/ToHopMon.java

--- src/main/java/dal/entities/ToHopMon.java
+++ src/main/java/dal/entities/ToHopMon.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package entity;
+package dal.entities;
 import java.io.Serializable;
-import javax.persistence.*;
+import jakarta.persistence.*;
 /**
  *
  * @author KIET
