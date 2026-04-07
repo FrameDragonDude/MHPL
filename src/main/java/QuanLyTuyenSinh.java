1cf8112df1b2e2f3cb644e60b2418d40b0cd09b5
Vo Dinh Kiet <vokiet2534@gmail.com>
2026-04-07 11:05:13 +0700

    tạo lại project dùng maven

---
+++ src/main/java/QuanLyTuyenSinh.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author KIET
+ */
+public class QuanLyTuyenSinh {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
